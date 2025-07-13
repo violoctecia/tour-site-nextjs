a877059ffe67031f29e9643fe5e8234ed55df533
Maxim Porfirev <violoctecia@MacBook-Pro-Maxim.local>
2025-07-13 04:43:49 +0300

    fix

--- src/components/blocks/Form.tsx
+++ src/components/blocks/Form.tsx
@@ -1,27 +1,46 @@
-// 'use client';
+'use client';
 
-
+import React, { useId, useState } from 'react';
 import styles from '@/styles/blocks/Hero.module.scss';
 import { InputMask } from '@react-input/mask';
 
 export default function ParentForm() {
+    const formId = useId();
+
+    const [form, setForm] = useState({
+        name: '',
+        phone: '',
+        checkbox: false
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type, checked } = e.target;
+
+        setForm((prev) => ({
+            ...prev,
+            [name]: type === 'checkbox' ? checked : value
+        }));
+    };
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
 
 
-    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     const { name, value, type, checked } = e.target;
-    //
-    //     setForm((prev) => ({
-    //         ...prev,
-    //         [name]: type === 'checkbox' ? checked : value
-    //     }));
-    // };
+        fetch("/__forms.html", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            // @ts-ignore
+            body: new URLSearchParams(formData).toString()
+        })
+            .then(() => alert('success'))
+            .catch(error => alert(error));
+    };
+
 
     return (
-        <form
-            name="lead"
-            method="POST"
-            data-netlify="true"
-        >
+        <form name="lead" method="POST" data-netlify="true" onSubmit={handleSubmit}>
             <input type="hidden" name="lead-form" value="lead" />
 
             <div className={styles.inputs}>
@@ -32,10 +51,19 @@ export default function ParentForm() {
                     placeholder="Как вас зовут?"
                     autoComplete="name"
                     required
-
+                    value={form.name}
+                    onChange={handleChange}
                 />
 
-
+                <InputMask
+                    mask="+7 (___) ___-__-__"
+                    replacement={{ _: /\d/ }}
+                    value={form.phone}
+                    name="phone"
+                    onChange={handleChange}
+                    required
+                    placeholder="+7 (999) 999-99-99"
+                />
 
                 <div className={styles.checkboxWrapper}>
                     <input
@@ -43,9 +71,10 @@ export default function ParentForm() {
                         id="checkbox"
                         name="checkbox"
                         required
-
+                        checked={form.checkbox}
+                        onChange={handleChange}
                     />
-                    <label htmlFor="checkbox">
+                    <label htmlFor="checkbox" onClick={e => e.preventDefault()}>
                         Я даю согласие на обработку персональных данных
                     </label>
                 </div>
